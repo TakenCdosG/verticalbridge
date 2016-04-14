ac5832c17744c4ab150e6899411f787e8e510448
Jonathan Caiaffa <jonathan.caiaffa@akendos.com>
2016-04-14 12:46:47 -0500

    Fixes BH-3

--- sites/all/themes/omega/ohm/templates/system/page--front.tpl.php
+++ sites/all/themes/omega/ohm/templates/system/page--front.tpl.php
@@ -73,7 +73,7 @@
 <div class="l-page">
 	<?php print $messages; ?>
 	<header class="l-header" role="banner">
-		<a href="<?php print $front_page; ?>" class="sublogo"><img src="/sites/default/files/logo-white.png" alt="Vertical Bridge" /></a>
+		<a href="<?php print $front_page; ?>" class="sublogo"><div class="image"></div></a>
 		<?php print render($page['header']); ?>
 		<a href="javascript:;" class="mobile-menu-btn center hidden">&equiv; Menu</a>
 		<?php print render($page['navigation']); ?>
