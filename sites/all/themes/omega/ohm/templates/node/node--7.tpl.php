0676ffd58d9ee1c9cb647cf4128f6046cc9e16ba
Jonathan Caiaffa <jonathan.caiaffa@akendos.com>
2016-04-29 16:19:10 -0500

    Fixes BH-10

---
+++ sites/all/themes/omega/ohm/templates/node/node--7.tpl.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: An array of node items. Use render($content) to print them all,
+ *   or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $user_picture: The node author's picture from user-picture.tpl.php.
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $name: Themed username of node author output from theme_username().
+ * - $node_url: Direct url of the current node.
+ * - $display_submitted: Whether submission information should be displayed.
+ * - $submitted: Submission information created from $name and $date during
+ *   template_preprocess_node().
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - node: The current template type, i.e., "theming hook".
+ *   - node-[type]: The current node type. For example, if the node is a
+ *     "Article" it would result in "node-article". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node-teaser: Nodes in teaser form.
+ *   - node-preview: Nodes in preview mode.
+ *   The following are controlled through the node publishing options.
+ *   - node-promoted: Nodes promoted to the front page.
+ *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser
+ *     listings.
+ *   - node-unpublished: Unpublished nodes visible only to administrators.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type, i.e. page, article, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $view_mode: View mode, e.g. 'full', 'teaser'...
+ * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * Field variables: for each field instance attached to the node a corresponding
+ * variable is defined, e.g. $node->body becomes $body. When needing to access
+ * a field's raw values, developers/themers are strongly encouraged to use these
+ * variables. Otherwise they will have to explicitly specify the desired field
+ * language, e.g. $node->body['en'], thus overriding any language negotiation
+ * rule that was previously applied.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ */
+?>
+
+<article<?php print $attributes; ?>>
+  <?php if (!empty($title_prefix) || !empty($title_suffix) || !$page): ?>
+    <header>
+      <?php print render($title_prefix); ?>
+      <?php if (!$page): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+    </header>
+  <?php endif; ?>
+
+  <?php if ($display_submitted): ?>
+    <footer class="node__submitted">
+      <?php print $user_picture; ?>
+      <p class="submitted"><?php print $submitted; ?></p>
+    </footer>
+  <?php endif; ?>
+
+  <div<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']); ?>
+      <div class="contact_webform">
+          <?php print render($content['webform']);?>
+      </div>
+      <div class="contact_content">
+          <div class="newsletter">
+            <!-- Begin MailChimp Signup Form -->
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+            #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+              We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div id="mc_embed_signup">
+            <form action="//verticalbridge.us12.list-manage.com/subscribe/post?u=2cc50706f31f5d4dc307fec05&amp;id=142d0d3317" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+            <h2>Sign Up for Updates</h2>
+            <div style="display:none" class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="mc-field-group input-group">
+                <ul><li><input type="checkbox" value="1" name="group[7449][1]" id="mce-group[7449]-7449-0"><label for="mce-group[7449]-7449-0">Media Coverage</label></li>
+            <li><input type="checkbox" value="2" name="group[7449][2]" id="mce-group[7449]-7449-1"><label for="mce-group[7449]-7449-1">VB Acquisitions</label></li>
+            <li><input type="checkbox" value="4" name="group[7449][4]" id="mce-group[7449]-7449-2"><label for="mce-group[7449]-7449-2">Press Releases</label></li>
+            </ul>
+            </div>
+            <div class="mc-field-group">
+            <label for="mce-EMAIL"></label>
+            <input type="email" value="Email" name="EMAIL" class="required email" id="mce-EMAIL">
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2cc50706f31f5d4dc307fec05_142d0d3317" tabindex="-1" value=""></div>
+                <input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button">
+            </div>
+            </div>
+            <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            </form>
+            </div>
+            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+            <!--End mc_embed_signup-->
+          </div>
+          <?php print render($content['body']);?>
+      </div>
+      
+	<?php if (!empty($sitemap)) { ?>
+		<?php foreach ($sitemap as $type => $entries) { ?>
+			<h3><?php echo $type; ?></h3>
+			<ul>
+			<?php foreach ($entries as $entry) { ?>
+				<li><a href="<?php print $entry['url']; ?>"><?php print $entry['text']; ?></a></li>
+			<?php } ?>
+			</ul>
+		<?php } ?>
+	<?php } ?>
+  </div>
+</article>
+<div style="clear: both;"></div>
