87bb71e985376769121da558ff44b2f1c8438bea
Jonathan Caiaffa <jonathan.caiaffa@akendos.com>
2016-05-17 17:46:11 -0500

    Started BH-98

--- sites/all/themes/omega/ohm/templates/node/node--basic-page-with-images.tpl.php
+++ sites/all/themes/omega/ohm/templates/node/node--basic-page-with-images.tpl.php
@@ -103,15 +103,15 @@
       hide($content['comments']);
       hide($content['links']); ?>
       <?php 
-      if(!empty($content['field_logos'])){
-        if($content['field_bottom_logos']==0){
+      
+      if($content['field_bottom_logos']==0){
           print '<div class="top_logos">'.render($content['field_logos']).'</div>';
           print render($content['body']);
         }
       }else{
           print render($content['body']);
           print '<div class="bottom_logos">'.render($content['field_logos']).'</div>';
-      }
+      
 
       ?>
           
