372f7aa8ec71556e021d18ae7090ce11d2ec09a6
Jonathan Caiaffa <jonathan.caiaffa@akendos.com>
2016-05-18 14:25:54 -0500

    Fixes BH-105

--- sites/all/themes/omega/ohm/templates/system/html.tpl.php
+++ sites/all/themes/omega/ohm/templates/system/html.tpl.php
@@ -185,13 +185,14 @@
       /*inner header*/
       $(document).ready(function(){
         var bpath = "<?php  echo !empty($base_path); ?>";
-        if($("body").hasClass("page-events") or $('.page-title').text() == "Events"){
+        var p_title = $('.page-title').text();
+        if($("body").hasClass("page-events") ||  p_title == "Events"){
           $('.internal-header').css("background-image","url(sites/all/themes/omega/ohm/images/events-bg.jpg)")
         }
-        if($("body").hasClass("page-press" or $('.page-title').text() == "Press")){
+        if($("body").hasClass("page-press" || p_title == "Press")){
           $('.internal-header').css("background-image","url(sites/all/themes/omega/ohm/images/press-bg.jpg)")
         }
-        if($("body").hasClass("page-about-us-newsroom" or $('.page-title').text() == "News")){
+        if($("body").hasClass("page-about-us-newsroom" || p_title == "News")){
           $('.internal-header').css("background-image","url("+bpath+"/sites/all/themes/omega/ohm/images/news-bg-2.jpg)")
         }
       });
